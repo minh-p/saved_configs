37d5fa7af9258f1b4bc7af760adae74bbb22e67d
minh-p <minhisverynormal@gmail.com>
2021-10-09 19:10:39 -0700

    added in

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+export PATH="$HOME/.cargo/bin:$PATH"
+export TERM=xterm-256color
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
---
+++ .config/wallpaper-set.sh
@@ -0,0 +1,3 @@
+feh --bg-scale ~/Pictures/wallpapers/cloud.jpg
+# killall -9 xwinwrap
+# xwinwrap -o 1.0 -fdt -d -s -st -sp -b -nf -ni -g 1920x1200+0+18  -- mpv -wid WID --loop -quiet ~/Videos/live-wallpapers/The-Journey-of-Elaina.mp4 & xwinwrap -o 1.0 -d -fdt -s -st -sp -b -nf -ni -g 1920x1080+1920+0  -- mpv -wid WID --loop -quiet ~/Videos/live-wallpapers/The-Journey-of-Elaina.mp4
